f5364fee9f4cb6a52c90c82d8921774d60e1a38b
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-03-17 13:54:44 +0530

    Debugging In TypeScript

--- dist/intro.js
+++ dist/intro.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log('Hello World');
 let age = 20;
+if (age > 18) {
+    console.log('You are an adult');
+}
+//# sourceMappingURL=intro.js.map
--- src/intro.ts
+++ src/intro.ts
@@ -1,3 +1,7 @@
 console.log('Hello World')
 
 let age: number = 20; 
+
+if(age > 18) {
+    console.log('You are an adult')
+}
